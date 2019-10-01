d53e1d08c82d450e387f370cd0714c8a26feb0a8
cjogles <jackson.ogles28@gmail.com>
2019-09-30 19:12:11 -0600

    finished MVP completely

--- js/index.js
+++ js/index.js
@@ -9,7 +9,7 @@ const siteContent = {
     "img-src": "img/logo.png"
   },
   "cta": {
-    "h1": "DOM Is Awesome",
+    "h1": "DOM \n Is \n Awesome",
     "button": "Get Started",
     "img-src": "img/header-img.png"
   },
@@ -28,7 +28,7 @@ const siteContent = {
   },
   "contact": {
     "contact-h4" : "Contact",
-    "address" : "123 Way 456 Street Somewhere, USA",
+    "address" : "123 Way 456 Street \n Somewhere, USA",
     "phone" : "1 (888) 888-8888",
     "email" : "sales@greatidea.io",
   },
@@ -48,18 +48,25 @@ circleImg.setAttribute('src', siteContent.cta["img-src"]);
 let rectangleImg = document.getElementById("middle-img");
 rectangleImg.setAttribute('src', siteContent["main-content"]["middle-img-src"]);
 
-let anchorList = document.getElementsByTagName("a");
-//console.log(anchorList);
-anchorList[0].textContent = siteContent["nav"]["nav-item-1"];
-anchorList[1].textContent = siteContent["nav"]["nav-item-2"];
-anchorList[2].textContent = siteContent["nav"]["nav-item-3"];
-anchorList[3].textContent = siteContent["nav"]["nav-item-4"];
-anchorList[4].textContent = siteContent["nav"]["nav-item-5"];
-anchorList[5].textContent = siteContent["nav"]["nav-item-6"];
+// let anchorList = document.getElementsByTagName("a");
+// //console.log(anchorList);
+// anchorList[0].textContent = siteContent["nav"]["nav-item-1"];
+// anchorList[1].textContent = siteContent["nav"]["nav-item-2"];
+// anchorList[2].textContent = siteContent["nav"]["nav-item-3"];
+// anchorList[3].textContent = siteContent["nav"]["nav-item-4"];
+// anchorList[4].textContent = siteContent["nav"]["nav-item-5"];
+// anchorList[5].textContent = siteContent["nav"]["nav-item-6"];
+// anchorList[0].style.color = "green";
+// anchorList[1].style.color = "green";
+// anchorList[2].style.color = "green";
+// anchorList[3].style.color = "green";
+// anchorList[4].style.color = "green";
+// anchorList[5].style.color = "green";
 
 const headSpot = document.querySelector("h1");
 //console.log(typeof headSpot);
 headSpot.textContent = siteContent.cta["h1"];
+headSpot.style.whiteSpace = "pre";
 //headSpot.style.columnCount = 3;
 //sheet.insertRule("h1 {word-spacing: <parent-width>}");
 const butt = document.querySelector("button");
@@ -80,35 +87,28 @@ pars[2].textContent = "Features content elementum magna eros, ac posuere elvit t
 pars[3].textContent = "Features content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.";
 pars[4].textContent = "Features content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.";
 pars[5].textContent = "123 Way 456 Street \n Somewhere, USA";
+pars[5].style.whiteSpace = "pre";
 pars[6].textContent = "1 (888) 888-8888";
 pars[7].textContent = "sales@greatidea.io";
 pars[8].textContent = "Copyright Great Idea! 2018";
 
 
 // NODE LIST EXAMPLE HOWWWWW!!???
-// let anchorList = document.querySelectorAll('a');
-// console.log(anchorList);
-// anchorList.forEach((element, i) => {
-//   //let j = i;
-//   //(function () {
-//     //console.log(element);
-//     if (element[i] === element[0]) {
-//       element[i].textContent = siteContent["nav"]["nav-item-1"];
-//     }
-//     else if (element[i] === element[1]) {
-//       element[i].textContent = siteContent["nav"]["nav-item-2"];
-//     }
-//     else if (element[i] === element[2]) {
-//       element[i].textContent = siteContent["nav"]["nav-item-3"];
-//     }
-//     else if (element[i] === element[3]) {
-//       element[i].textContent = siteContent["nav"]["nav-item-4"];
-//     }
-//     else if (element[i] === element[4]) {
-//       element[i].textContent = siteContent["nav"]["nav-item-5"];
-//     }
-//     else if (element[i] === element[5]) {
-//       element[i].textContent = siteContent["nav"]["nav-item-6"];
-//     }
-//   //})
-// });
+let anchorList = document.querySelectorAll('a');
+console.log(anchorList);
+anchorList.forEach((element, i) => {
+  element.textContent = siteContent["nav"][`nav-item-${i+1}`];
+});
+
+var parent = anchorList[0];
+var newTag = document.createElement("a");
+newTag.setAttribute('href', "nba.com");
+newTag.innerText = "TAG ONE    ";
+parent.prepend(newTag);
+
+var parent2 = anchorList[anchorList.length-1];
+var newTag2 = document.createElement("a");
+newTag2.setAttribute('href', "nba.com");
+newTag2.innerText = "TAG TWO";
+parent2.append(newTag2);
+
